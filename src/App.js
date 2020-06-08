056c4d727b0c86409bdbb9f408d3e4d510a05244
genie <hee.1.63m@gmail.com>
2020-06-08 20:31:25 +0900

    Ready to create movie review app.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,19 @@
 import React from "react";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("constructor execute");
-  }
-
-  componentDidMount() {
-    console.log("component rendered");
-  }
-
-  componentDidUpdate() {
-    console.log("component updated");
-  }
-
-  componentWillUnmount() {
-    console.log("component unmounted");
-  }
-
   state = {
-    count: 0,
+    loading: true,
+    movies: [],
   };
-
-  increase = () => {
-    this.setState((state) => ({
-      count: state.count + 1,
-    }));
-  };
-
-  decrease = () => {
-    this.setState((state) => ({
-      count: state.count - 1,
-    }));
-  };
-
+  componentDidMount() {
+    //movie data loading
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 6000); // set timeout 6sec
+  }
   render() {
-    console.log("render execute");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}.</h1>
-        <button onClick={this.increase}>+</button>
-        <button onClick={this.decrease}>-</button>
-      </div>
-    );
+    const { loading } = this.state;
+    return <div>{loading ? "Loading..." : "Loaded"}</div>;
   }
 }
 
