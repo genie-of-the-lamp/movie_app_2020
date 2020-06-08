ac7d3544b2a6493348ae7331514fbbb1889a4b7d
genie <hee.1.63m@gmail.com>
2020-06-08 20:22:49 +0900

    Create counter and Study for lifecycle of component.

--- src/App.js
+++ src/App.js
@@ -1,61 +1,49 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-function Movie({ subject, image, rating }) {
-  return (
-    <div>
-      <h4>I like {subject} S2</h4>
-      <h5>rating: {rating}</h5>
-      <img src={image} width="300px" alt={subject} />
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor execute");
+  }
 
-Movie.propTypes = {
-  subject: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
+  componentDidMount() {
+    console.log("component rendered");
+  }
 
-const likedMovie = [
-  {
-    id: 1,
-    subject: "Memento",
-    image:
-      "http://ticketimage.interpark.com/Movie/still_image/V08/V0801339p_01.gif",
-    rating: 5,
-  },
-  {
-    id: 2,
-    subject: "Parosite",
-    image:
-      "https://movie-phinf.pstatic.net/20190528_36/1559024198386YVTEw_JPEG/movie_image.jpg",
-    rating: 4,
-  },
-  {
-    id: 3,
-    subject: "CUBE",
-    image:
-      "https://movie-phinf.pstatic.net/20111222_68/1324503834866ARyKw_JPEG/movie_image.jpg",
-    rating: 4.5,
-  },
-];
+  componentDidUpdate() {
+    console.log("component updated");
+  }
 
-function App() {
-  const movies = likedMovie.map((item) => (
-    <Movie
-      subject={item.subject}
-      image={item.image}
-      key={item.id}
-      rating={item.rating}
-    />
-  ));
-  return (
-    <div>
-      <h1>Header</h1>
-      {movies}
-    </div>
-  );
+  componentWillUnmount() {
+    console.log("component unmounted");
+  }
+
+  state = {
+    count: 0,
+  };
+
+  increase = () => {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  };
+
+  decrease = () => {
+    this.setState((state) => ({
+      count: state.count - 1,
+    }));
+  };
+
+  render() {
+    console.log("render execute");
+    return (
+      <div>
+        <h1>The number is: {this.state.count}.</h1>
+        <button onClick={this.increase}>+</button>
+        <button onClick={this.decrease}>-</button>
+      </div>
+    );
+  }
 }
 
 export default App;
