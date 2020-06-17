ce02bd62599734d000d9b4037decb97e747e3e37
genie <hee.1.63m@gmail.com>
2020-06-17 21:35:18 +0900

    Updates

---
+++ precache-manifest.197f4b5a214e231c4a61bc9946ff50e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de116956d049c7362574e131c39b78c1",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "41468fd87e33de47ee40",
+    "url": "/movie_app_2020/static/css/main.efa2e1ac.chunk.css"
+  },
+  {
+    "revision": "5aacfab4a584596c2469",
+    "url": "/movie_app_2020/static/js/2.e730c905.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.e730c905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41468fd87e33de47ee40",
+    "url": "/movie_app_2020/static/js/main.015fbebb.chunk.js"
+  },
+  {
+    "revision": "01aae0007e1cfd873da8",
+    "url": "/movie_app_2020/static/js/runtime-main.482f7179.js"
+  }
+]);
