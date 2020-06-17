ee88b5472cdf8f022749554e82adaabdd8252de7
genie <hee.1.63m@gmail.com>
2020-06-17 21:03:33 +0900

    Create Navigation component with Link.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import About from "./routes/About";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
     </HashRouter>
